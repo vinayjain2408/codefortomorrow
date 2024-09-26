36bc01db4a577b639726897fe6096fc5644fc636
vinayjain2408 <vinay98292jain@gmail.com>
2024-09-26 17:09:10 +0530

    new update

--- src/page/LeftSide.js
+++ src/page/LeftSide.js
@@ -1,208 +1,231 @@
 import React, { useState } from 'react';
 import { Avatar, Card, Button, Drawer, Form, Input } from 'antd';
 import "./LeftSide.css"
-function LeftSide({setViewMode}) {
-  const [isDrawerVisible, setIsDrawerVisible] = useState(false); // State to control drawer visibility
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    address: '',
-    country: '',
-    email: '',
-    phone: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
+function LeftSide({ setViewMode }) {
+    const [isDrawerVisible, setIsDrawerVisible] = useState(false); // State to control drawer visibility
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        address: '',
+        country: '',
+        email: '',
+        phone: '',
     });
-  };
 
-  const handleSubmit = (values) => {
-    if(formData?.address !== '' && formData?.country !== '' && formData?.email !== '' && formData?.firstName !== ''&& formData?.lastName !== ''&& formData?.phone !== '' ){
-        console.log('Feedback Submitted:', values);
-        console.log('Feedback Submitted:', formData);
-        setIsDrawerVisible(false);
+    const handleChange = (e) => {
+        const { name, value } = e.target;
         setFormData({
-          firstName: '',
-          lastName: '',
-          address: '',
-          country: '',
-          email: '',
-          phone: '',
+            ...formData,
+            [name]: value,
         });
-    }
+    };
 
-  };
-  const showDrawer = () => {
-    setIsDrawerVisible(true);
-  };
+    const handleSubmit = (values) => {
+        if (formData?.address !== '' && formData?.country !== '' && formData?.email !== '' && formData?.firstName !== '' && formData?.lastName !== '' && formData?.phone !== '') {
+            console.log('Feedback Submitted:', values);
+            console.log('Feedback Submitted:', formData);
+            setIsDrawerVisible(false);
+            setFormData({
+                firstName: '',
+                lastName: '',
+                address: '',
+                country: '',
+                email: '',
+                phone: '',
+            });
+        }
 
-  const onClose = () => {
-    setIsDrawerVisible(false);
-  };
+    };
+    const showDrawer = () => {
+        setIsDrawerVisible(true);
+    };
 
-  return (
-    <div>
-      <Card
-        style={{
-          minWidth: 300,
-        }}
-      >
-        <Card.Meta
-          avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />}
-          title="Hi Reader, Here's your News!"
-        />
-      </Card>
-      <Card className="toggle-card">
-        <p>View Toggle</p>
-        <Button.Group className="toggle-buttons">
-          <Button onClick={() => setViewMode('grid')}>Grid View</Button>
-          <Button onClick={() => setViewMode('list')}>List View</Button>
-        </Button.Group>
-      </Card>
-      <Card
-        style={{
-          width: '100%',
-          marginTop: '20px',
-        }}
-      >
-        <h3>Have a Feedback?</h3>
-        <Button
-          type="primary"
-          size="large"
-          onClick={showDrawer} // Show the drawer when clicked
-          style={{ backgroundColor: '#ff7f7f', border: 'none' }}
-        >
-          We're Listening!
-        </Button>
-      </Card>
+    const onClose = () => {
+        setIsDrawerVisible(false);
+    };
 
-      {/* Feedback Drawer */}
-
-<Drawer
-  title="Feedback"
-  placement="left"
-  onClose={onClose}
-  width={700}
-  visible={isDrawerVisible}
->
-  <div className="leftContainer">
-    <div className="leftSide">
-    <Card
-        style={{
-          width: "200px",
-          padding: "0"
-        }}
-      >
-        <div className='cardTop'>
-        <Card.Meta
-          avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />}
-        //   title="Hi Reader, Here's your News!"
-        />
+    return (
         <div>
-        <h3>Hi Reader</h3>
-        <p>Here's your News!</p>
-        </div>
-        </div>
-      </Card>
-
-      <Card
-        style={{
-          width: '80%',
-          marginTop: '20px',
-        }}
-      >
-        <h3 className='wigthBold'>Have a Feedback?</h3>
-        <Button
-          type="primary"
-          size="large"
-          onClick={showDrawer} // Show the drawer when clicked
-          style={{ backgroundColor: '#ff7f7f', border: 'none' }}
-        >
-          We're Listening!
-        </Button>
-      </Card>
-    </div>
-    <div className="rightSide">
-      <h2>Thank you so much for taking the time!</h2>
-      <p>Please provide the below details!</p>
-
-      <Form layout="vertical" onFinish={handleSubmit}>
-            <Form.Item label="First Name" required>
-              <Input 
-                name="firstName" 
-                value={formData.firstName} 
-                onChange={handleChange} 
-              />
-            </Form.Item>
-
-            <Form.Item label="Last Name" required>
-              <Input 
-                name="lastName" 
-                value={formData.lastName} 
-                onChange={handleChange} 
-              />
-            </Form.Item>
-
-            <Form.Item label="Address" required>
-              <Input.TextArea 
-                name="address"
-                placeholder="Enter your full Postal Address" 
-                value={formData.address} 
-                onChange={handleChange} 
-              />
-            </Form.Item>
-
-            <Form.Item label="Country" required>
-              <Input 
-                name="country" 
-                value={formData.country} 
-                onChange={handleChange} 
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Email ID"
-              required
-              validateStatus={formData.email ? '' : 'error'}
-              help={formData.email ? '' : 'Please enter a valid e-mail'}
+            {/* <Card
+                style={{
+                    minWidth: 300,
+                }}
             >
-              <Input 
-                name="email" 
-                placeholder="example@sample.com" 
-                value={formData.email} 
-                onChange={handleChange} 
-              />
-            </Form.Item>
+                <Card.Meta
+                    avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />}
+                    title="Hi Reader, Here's your News!"
+                />
+            </Card> */}
 
-            <Form.Item
-              label="Phone Number"
-              required
-              validateStatus={formData.phone ? '' : 'error'}
-              help={formData.phone ? '' : 'Please enter a valid number'}
+            <Card
+                style={{
+                    width: "250px",
+                    padding: "0",
+                    margin:"20px"
+                }}
             >
-              <Input 
-                name="phone" 
-                addonBefore="+91" 
-                placeholder="123456789" 
-                value={formData.phone} 
-                onChange={handleChange} 
-              />
-            </Form.Item>
+                <div className='cardTop'>
+                    <Card.Meta
+                        avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />}
 
-            <Button type="primary" htmlType="submit" block>
-              Submit Feedback
-            </Button>
-          </Form>
-    </div>
-  </div>
-</Drawer>
+                    />
+                    <div>
+                        <h3>Hi Reader</h3>
+                        <p>Here's your News!</p>
+                    </div>
+                </div>
+            </Card>
+            <Card className="toggle-card" style={{
+                    width: "300px",
+                    padding: "0",
+                    margin:"20px"
+                }}>
+                <p>View Toggle</p>
+                <Button.Group className="toggle-buttons">
+                    <Button onClick={() => setViewMode('grid')}>Grid View</Button>
+                    <Button onClick={() => setViewMode('list')}>List View</Button>
+                </Button.Group>
+            </Card>
+            <Card
+                style={{
+                    width: '100%',
+                    marginTop: '20px',
+                }}
+            >
+                <h3>Have a Feedback?</h3>
+                <Button
+                    type="primary"
+                    size="large"
+                    onClick={showDrawer} // Show the drawer when clicked
+                    style={{ backgroundColor: '#ff7f7f', border: 'none' }}
+                >
+                    We're Listening!
+                </Button>
+            </Card>
 
-    </div>
-  );
+            {/* Feedback Drawer */}
+
+            <Drawer
+                title="Feedback"
+                placement="left"
+                onClose={onClose}
+                width={700}
+                visible={isDrawerVisible}
+            >
+                <div className="leftContainer">
+                    <div className="leftSide">
+                        <Card
+                            style={{
+                                width: "200px",
+                                padding: "0"
+                            }}
+                        >
+                            <div className='cardTop'>
+                                <Card.Meta
+                                    avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />}
+
+                                />
+                                <div>
+                                    <h3>Hi Reader</h3>
+                                    <p>Here's your News!</p>
+                                </div>
+                            </div>
+                        </Card>
+
+                        <Card
+                            style={{
+                                width: '80%',
+                                marginTop: '20px',
+                            }}
+                        >
+                            <h3 className='wigthBold'>Have a Feedback?</h3>
+                            <Button
+                                type="primary"
+                                size="large"
+                                onClick={showDrawer} // Show the drawer when clicked
+                                style={{ backgroundColor: '#ff7f7f', border: 'none' }}
+                            >
+                                We're Listening!
+                            </Button>
+                        </Card>
+                    </div>
+                    <div className="rightSide">
+                        <h2>Thank you so much for taking the time!</h2>
+                        <p>Please provide the below details!</p>
+
+                        <Form layout="vertical" onFinish={handleSubmit}>
+                            <Form.Item label="First Name" required>
+                                <Input
+                                    name="firstName"
+                                    value={formData.firstName}
+                                    onChange={handleChange}
+                                />
+                            </Form.Item>
+
+                            <Form.Item label="Last Name" required>
+                                <Input
+                                    name="lastName"
+                                    value={formData.lastName}
+                                    onChange={handleChange}
+                                />
+                            </Form.Item>
+
+                            <Form.Item label="Address" required>
+                                <Input.TextArea
+                                    name="address"
+                                    placeholder="Enter your full Postal Address"
+                                    value={formData.address}
+                                    onChange={handleChange}
+                                />
+                            </Form.Item>
+
+                            <Form.Item label="Country" required>
+                                <Input
+                                    name="country"
+                                    value={formData.country}
+                                    onChange={handleChange}
+                                />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Email ID"
+                                required
+                                validateStatus={formData.email ? '' : 'error'}
+                                help={formData.email ? '' : 'Please enter a valid e-mail'}
+                            >
+                                <Input
+                                    name="email"
+                                    placeholder="example@sample.com"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                />
+                            </Form.Item>
+
+                            <Form.Item
+                                label="Phone Number"
+                                required
+                                validateStatus={formData.phone ? '' : 'error'}
+                                help={formData.phone ? '' : 'Please enter a valid number'}
+                            >
+                                <Input
+                                    name="phone"
+                                    addonBefore="+91"
+                                    placeholder="123456789"
+                                    value={formData.phone}
+                                    onChange={handleChange}
+                                />
+                            </Form.Item>
+
+                            <Button type="primary" htmlType="submit" block>
+                                Submit Feedback
+                            </Button>
+                        </Form>
+                    </div>
+                </div>
+            </Drawer>
+
+        </div>
+    );
 }
 
 export default LeftSide;
--- src/page/RightSide.js
+++ src/page/RightSide.js
@@ -1,14 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { List, Card, Avatar, Pagination, Modal, Row, Col } from 'antd';
 import './RightSide.css';
+import { CloseOutlined } from '@ant-design/icons';
 import axios from "axios";
+
 const { Meta } = Card;
+
 function RightSide({ viewMode }) {
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [pageSize, setPageSize] = useState(6);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState(null);
+
   const fetchData = async () => {
     const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
     setData(response?.data);
@@ -26,6 +30,7 @@ function RightSide({ viewMode }) {
     setCurrentPage(page);
     setPageSize(pageSize);
   };
+
   const showModal = (item) => {
     setSelectedItem(item);
     setIsModalVisible(true);
@@ -34,37 +39,52 @@ function RightSide({ viewMode }) {
   const handleCancel = () => {
     setIsModalVisible(false);
   };
-  console.log("paginatedData",paginatedData)
+
+  const onClose = (id) => {
+    const newData = data?.filter(item => item?.id !== id);
+    setData(newData);
+    setIsModalVisible(false);
+    setSelectedItem(null);
+  };
+
   return (
     <div className="right-container">
       {
-        viewMode === "grid" ?
-        <div className="card-grid">
-        <Row gutter={[16, 16]} justify="center">
-          {paginatedData?.map((item, index) => (
-            <Col key={index} xs={24} sm={12} md={8} lg={8}> {/* Adjust based on screen size */}
-              <Card
-                style={{
-                  width: '100%',
-                  height:'400px'
-                }}
-            
-              >
-                <Meta
-                  avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
-                  title={item?.title}
-                />
-                <p>{item?.body}</p>
-                <img
-                    alt="example"
-                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-                  />
-              </Card>
-            </Col>
-          ))}
-        </Row>
-      </div>
-          :
+        viewMode === "grid" ? (
+          <div className="card-grid">
+            <Row gutter={[16, 16]} justify="center">
+              {paginatedData?.map((item, index) => (
+                <Col key={index} xs={24} sm={12} md={8} lg={8}>
+                  <Card
+                    style={{
+                      width: '100%',
+                      height: '400px'
+                    }}
+                    onClick={() => showModal(item)}
+                  >
+                    <Meta
+                      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
+                      title={item?.title}
+                    />
+                    <CloseOutlined 
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        onClose(item?.id);
+                      }} 
+                      style={{ cursor: 'pointer' }} 
+                      className='iconData' 
+                    />
+                    <p>{item?.body}</p>
+                    <img
+                      alt="example"
+                      src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                    />
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          </div>
+        ) : (
           <List
             itemLayout="horizontal"
             dataSource={paginatedData}
@@ -75,14 +95,21 @@ function RightSide({ viewMode }) {
                     avatar={<Avatar src="https://via.placeholder.com/50" />}
                     title={item.title}
                   />
+                  <CloseOutlined 
+                    onClick={(e) => {
+                      e.stopPropagation(); 
+                      onClose(item?.id);
+                    }} 
+                    style={{ cursor: 'pointer' }} 
+                    className='iconData' 
+                  />
                   <div>{item.body}</div>
                 </Card>
               </List.Item>
             )}
           />
+        )
       }
-
-
 
       {selectedItem && (
         <Modal
